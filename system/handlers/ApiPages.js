8cc3b2671645341ddaa35c1fafc2f1f3d71c162d
Gilles Perreymond <gperreymond@gmail.com>
2017-10-08 22:38:32 +0200

    api pages

--- system/handlers/ApiPages.js
+++ system/handlers/ApiPages.js
@@ -1,22 +1,6 @@
-const path = require('path')
-const Boom = require('boom')
-const fse = require('fs-extra')
-
 const handler = (request, reply) => {
-  // get authorize pages to load
-  request.server.methods.pages(request.path, (error, pages) => {
-    if (error) return reply(Boom.notFound('Page not found!'))
-    // get current page content
-    let requestLabel = request.path.substr(1)
-    if (requestLabel === '') requestLabel = 'home'
-    if (!pages[requestLabel]) return reply(Boom.notFound('Page not found!'))
-    let page = pages[requestLabel]
-    const stream = fse.createReadStream(path.resolve(__dirname, '../../pages', page.filepath))
-    stream.on('data', (chunk) => {
-      stream.close()
-      page.content = chunk.toString()
-      reply.view('default/index', page)
-    })
+  reply({
+    pages: []
   })
 }
 
