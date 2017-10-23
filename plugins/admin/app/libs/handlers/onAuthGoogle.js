e391eb68ae68388bb0318639274c061d2627229b
Gilles Perreymond <gperreymond@gmail.com>
2017-10-23 20:40:08 +0200

    admin login all done

--- plugins/admin/app/libs/handlers/onAuthGoogle.js
+++ plugins/admin/app/libs/handlers/onAuthGoogle.js
@@ -4,7 +4,7 @@ const debug = Debug('nova:admin:actions:authGoogle')
 
 const handler = (context) => {
   debug('start')
-  window.location = '/admin/login'
+  window.location = '/admin/auth/login'
 }
 
 export default handler
