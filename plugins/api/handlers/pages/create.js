0bc224412d68159655394862f3e62e2eb23f050d
Gilles Perreymond <gperreymond@gmail.com>
2017-10-29 19:19:15 +0100

    create page done

--- plugins/api/handlers/pages/create.js
+++ plugins/api/handlers/pages/create.js
@@ -1,5 +1,16 @@
+const path = require('path')
+const fse = require('fs-extra')
+const uuid = require('uuid')
+
+const list = require('./list')
+
 const handler = async (request, reply) => {
-  reply({account: true})
+  const filepath = path.resolve(__dirname, '../../../../user/pages', '00-' + uuid.v4(), 'default.md')
+  fse.ensureFileSync(filepath)
+  const stream = fse.createWriteStream(filepath)
+  stream.write('# Page')
+  stream.end()
+  list(request, reply)
 }
 
 module.exports = handler
