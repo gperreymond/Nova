388bf3d769d450915263b9d204ac0b7179ea02ef
Gilles Perreymond <gperreymond@gmail.com>
2017-11-03 21:00:12 +0100

    module in page content

--- plugins/api/pages/read/handler.js
+++ plugins/api/pages/read/handler.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fse = require('fs-extra')
 const Boom = require('boom')
+const YAML = require('yamljs')
 
 const handler = async (request, reply) => {
   const pages = request.pre.pages
@@ -10,6 +11,18 @@ const handler = async (request, reply) => {
     const chunk = fse.readFileSync(path.resolve(__dirname, '../../user/pages', page.filepath))
     let content = chunk.toString().split('---')
     page.content = content[2] || content[0]
+    Object.keys(page.modules).map(module => {
+      const chunkModule = fse.readFileSync(page.modules[module])
+      let contentModule = chunkModule.toString().split('---')
+      let metadataModule = { type: false }
+      if (contentModule[1]) {
+        metadataModule = YAML.parse(contentModule[1].toString())
+      }
+      page.modules[module] = {
+        metadata: metadataModule,
+        content: contentModule[2] || contentModule[0]
+      }
+    })
     reply(page)
   } else {
     reply(Boom.notFound('Page not found!'))
--- plugins/cms/handler.js
+++ plugins/cms/handler.js
@@ -1,5 +1,8 @@
 const _ = require('lodash')
+const path = require('path')
+const fse = require('fs-extra')
 const Boom = require('boom')
+const YAML = require('yamljs')
 
 const handler = (request, reply) => {
   const pages = request.pre.pages.dataProvider
@@ -9,19 +12,25 @@ const handler = (request, reply) => {
   const currentIndex = _.findIndex(pages, function (item) { return item.name === requestName })
   if (currentIndex === -1) return reply(Boom.notFound('Page not found!'))
   let page = pages[currentIndex]
-  page.content = ''
-  /* const chunk = fse.readFileSync(path.resolve(__dirname, '../../user/pages', page.filepath))
+  // get page content
+  const chunk = fse.readFileSync(path.resolve(__dirname, '../../user/pages', page.filepath))
   let content = chunk.toString().split('---')
   page.content = content[2] || content[0]
-  // is there any modules ?
-  if (page.metadata && page.metadata.modules) {
-    page.metadata.modules.map(module => {
-      let chunkModule = fse.readFileSync(path.resolve(__dirname, '../../user/pages', page.filepath, '../modules', module.path + '.md'))
-      let contentModule = chunkModule.toString().split('---')
-      const regex = /\[modules:showcase_youtube]/g
-      page.content = page.content.replace(regex, contentModule[2] || contentModule[0])
-    })
-  } */
+  Object.keys(page.modules).map(module => {
+    const chunkModule = fse.readFileSync(page.modules[module])
+    let contentModule = chunkModule.toString().split('---')
+    let metadataModule = { type: false }
+    if (contentModule[1]) {
+      metadataModule = YAML.parse(contentModule[1].toString())
+    }
+    page.modules[module] = {
+      metadata: metadataModule,
+      content: contentModule[2] || contentModule[0]
+    }
+    // remplace module in page content
+    const regex = new RegExp('\\[modules:' + module + ']', 'g')
+    page.content = page.content.replace(regex, page.modules[module].content)
+  })
   // the reply
   reply.view('themes/' + page.theme + '/index', page)
 }
